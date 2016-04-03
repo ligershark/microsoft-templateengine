f73d0b03246dc6cd039def328c9505fa23e22e70
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-04-03 14:11:49 -0700

    updating appv for more trials

--- appveyor.ps1
+++ appveyor.ps1
@@ -5,8 +5,6 @@ $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 
 <#
 try{
-    
-
     if($env:APPVEYOR_REPO_BRANCH -eq 'release' -and ([string]::IsNullOrWhiteSpace($env:APPVEYOR_PULL_REQUEST_NUMBER) )) {
         . $buildFile.FullName -publishToNuget
     }
@@ -20,14 +18,13 @@ catch{
 }
 #>
 
-$channelList = 'future','preview',$null
+$channelList = @('future','preview',$null)
 $feedList = 'https://dotnet.myget.org/f/dotnet-cli','https://api.nuget.org/v3/index.json'
 $installUrlList = 'https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1','https://raw.githubusercontent.com/sayedihashimi/cli/issue2236/scripts/obtain/install.ps1'
 $originalPath = $env:Path
 
 foreach($installUrl in $installUrlList){
     # rest path to original
-    $env:path = $originalPath
     'Trying with installer from [{0}]' -f $installUrlList | Write-Host
     try{
         $installdir = "$env:LOCALAPPDATA\Microsoft\dotnet"
@@ -35,16 +32,31 @@ foreach($installUrl in $installUrlList){
             Remove-Item $installdir -Recurse
         }
 
-        . $buildFile.FullName -installOnly
+        #$env:path = $originalPath
+        #. $buildFile.FullName -installOnly -dotnetInstallChannel $channel
 
         foreach($channel in $channelList){
-            'Trying build with [channel={0},nugeturl=<null>]' -f $channel | Write-Host
-            . $buildFile.FullName -dotnetInstallChannel $channel -dotnetNugetFeedSource $null -SkipInstallDotNet -onlyBuildDOtnetProjects
+            $env:path = $originalPath
+            @'
+ ****
+ **** Installing
+ ****   - url    ={0}
+ ****   - channel={1}
+ ****
+'@ -f $installUrl,$channel | Write-Host -ForegroundColor Green
+            . $buildFile.FullName -installOnly -dotnetNugetFeedSource $channel -dotnetInstallUrl $installUrl
 
             foreach($feed in $feedList){
                 try{
-                    'Trying build with [channel={0},nugeturl={1}]' -f $channel,$feed | Write-Host
-                    . $buildFile.FullName -dotnetInstallChannel $channel -dotnetNugetFeedSource $feed -SkipInstallDotNet -onlyBuildDOtnetProjects
+@'
+ >>
+ >> Trying build with 
+ >>    - channel={0}
+ >>    - nugeturl={1}
+ >>    - installUrl={2}
+ >>
+'@ -f $channel,$feed,$installUrl | Write-Host -ForegroundColor Green
+                    . $buildFile.FullName -dotnetNugetFeedSource $feed -SkipInstallDotNet -onlyBuildDOtnetProjects
                 }
                 catch{
                     ( 'Build error {0} {1}' -f $_.Exception, (Get-PSCallStack|Out-String) ) | Write-Warning
@@ -53,7 +65,7 @@ foreach($installUrl in $installUrlList){
         }
     }
     catch{
-        'Unable to install from url [{0}]' -f $installUrlList | Write-Warning
+        'Unable to install from url [{0}]. Error: {1}' -f $installUrl,$_.Exception | Write-Warning
     }
 }
 
